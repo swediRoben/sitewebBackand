a4455333e39ce19c22925bf6396ed62e6bd350ab
swediRoben <farajampenda43@gmail.com>
2023-10-13 16:47:34 +0200

    new spring boot

---
+++ siteweb/src/main/java/com/siteweb/siteweb/SitewebApplication.java
@@ -0,0 +1,14 @@
+package com.siteweb.siteweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitewebApplication.class, args);
+	   
+	}
+
+}
