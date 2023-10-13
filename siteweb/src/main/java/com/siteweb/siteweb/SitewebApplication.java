29f1f1a5d6e93944fc5c0305b9b23bcbbd3d786e
swediRoben <farajampenda43@gmail.com>
2023-10-13 17:15:52 +0200

    java

--- siteweb/src/main/java/com/siteweb/siteweb/SitewebApplication.java
+++ siteweb/src/main/java/com/siteweb/siteweb/SitewebApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SitewebApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(SitewebApplication.class, args);
-	   
+		S
 	}
 
 }
