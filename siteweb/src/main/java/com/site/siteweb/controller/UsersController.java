3da729c99e26f01c264d053893653f03d74120eb
msfaraja <msfaraja@asystresources.com>
2023-10-20 12:06:35 +0200

    del

--- siteweb/src/main/java/com/site/siteweb/controller/UsersController.java
+++ siteweb/src/main/java/com/site/siteweb/controller/UsersController.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +34,40 @@ public class UsersController {
                 } else {
                         return new ResponseEntity<>(
                                         new ResponseHelper(MessageHelper.success(new Locale(localeString)),
+                                                        false),
+                                        HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+
+        }
+
+       @PutMapping("/{id}")
+      public ResponseEntity<Object> update(@RequestHeader(name = "Accept-Language", required = false) String localeString,@PathVariable("id") Long id,@RequestBody UsersDto user) { 
+                boolean data = service.upDate(id,user);   
+                if (data) { 
+                        return new ResponseEntity<>(
+                                        new ResponseHelper(MessageHelper.success(new Locale(localeString)),true),HttpStatus.CREATED);
+                } else {
+                        return new ResponseEntity<>(
+                                        new ResponseHelper(MessageHelper.success(new Locale(localeString)),
+                                                        false),
+                                        HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+
+        }
+
+
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Object> daelete(@PathVariable("id") Long id,@RequestHeader(name = "Accept-Language", required = false) String localeString ) { 
+                boolean data = service.delete(id);  
+                if (data) {
+                    
+                        return new ResponseEntity<>(
+                                        new ResponseHelper(MessageHelper.deletedSuccessFully(new Locale(localeString)),
+                                                        true),
+                                        HttpStatus.CREATED);
+                } else {
+                        return new ResponseEntity<>(
+                                        new ResponseHelper(MessageHelper.deletedElementUsed(new Locale(localeString)),
                                                         false),
                                         HttpStatus.INTERNAL_SERVER_ERROR);
                 }
