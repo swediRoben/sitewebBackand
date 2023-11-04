de4cebfb40f65c126cf8e0591ed148bd668cbeaf
swediRoben <farajampenda43@gmail.com>
2023-11-04 18:59:01 +0200

    bac

--- src/main/java/com/site/siteweb/SitewebApplication.java
+++ src/main/java/com/site/siteweb/SitewebApplication.java
@@ -3,8 +3,6 @@ package com.site.siteweb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 import com.site.siteweb.service.MailSenderService;
 
